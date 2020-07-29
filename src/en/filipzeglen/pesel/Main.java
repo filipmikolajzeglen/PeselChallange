ef8de3e50b05ffac20379e1aac2174962dc5e17c
Filip Mikołaj Żegleń <62224199+filipmikolajzeglen@users.noreply.github.com>
2020-07-30 00:04:16 +0200

    Update Main.java

--- src/en/filipzeglen/pesel/Main.java
+++ src/en/filipzeglen/pesel/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-        // ENTER THE PESEL NUMBER
         Scanner scanner = new Scanner(System.in);
         System.out.print(" Enter your PESEL number: ");
         String enterNrPesel = scanner.next();
